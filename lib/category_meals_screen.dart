201017413dafb01cf1b4752e95fafc318a61970c
SHIVAM YADAV <shivamyadavskn@gmail.com>
2022-12-09 01:07:03 +0530

    Today Updates

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
-  const CategoryMealsScreen(
-      {super.key, required this.categoryId, required this.categoryTitle});
+  const CategoryMealsScreen({super.key});
+  // final String categoryId;
+  // final String categoryTitle;
+  // const CategoryMealsScreen(
+  //     {super.key, required this.categoryId, required this.categoryTitle});
   @override
   Widget build(BuildContext context) {
+final routeArgs=ModalRoute.of(context)?.settings.arguments as Map<String,String>;
+final categoryTitle=routeArgs['title'];
+final categoryId=routeArgs['id'];
     return Scaffold(
       appBar: AppBar(
-        title: Text(categoryTitle),
+        title: Text(categoryTitle!),
       ),
       body: const Center(
         child: Text('Category meal'),
