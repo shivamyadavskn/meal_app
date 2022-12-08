a5daa9f78de8741fa2e483fdb5d1878e4743c86d
SHIVAM YADAV <shivamyadavskn@gmail.com>
2022-12-09 01:58:10 +0530

    Today Updates

--- lib/model/meal.dart
+++ lib/model/meal.dart
@@ -1,3 +1,41 @@
-class Meal{
+enum Complexicity {
+  easy,
+  challenging,
+  hard,
+}
 
+enum Affordablity {
+  affordable,
+  pricey,
+  luxurious,
+}
+
+class Meal {
+  final String id;
+  final List<String> categories;
+  final String title;
+  final String imageUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+  final Complexicity complexicity;
+  final Affordablity affordablity;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+  Meal(
+      {required this.id,
+      required this.categories,
+      required this.title,
+      required this.imageUrl,
+      required this.ingredients,
+      required this.steps,
+      required this.duration,
+      required this.complexicity,
+      required this.affordablity,
+      required this.isGlutenFree,
+      required this.isLactoseFree,
+      required this.isVegan,
+      required this.isVegetarian});
 }
